3a4d58f00ef3e46f28e596562e2e851d34398e3f
Joe Simpson <me@kennydude.me>
2024-04-08 21:00:03 +0100

    Author listing in a working position

--- AuthorListingPageHandler.php
+++ AuthorListingPageHandler.php
@@ -5,6 +5,7 @@ namespace APP\plugins\generic\authorListing;
 use APP\core\Request;
 use APP\handler\Handler;
 use APP\template\TemplateManager;
+use Illuminate\Support\Facades\DB;
 use PKP\controllers\page\PageHandler;
 use PKP\security\authorization\ContextAccessPolicy;
 use PKP\security\authorization\ContextRequiredPolicy;
@@ -26,6 +27,22 @@ class AuthorListingPageHandler extends Handler {
     public function index($args, $request)
     {
         $templateMgr = TemplateManager::getManager($request);
+
+        $context = $request->getContext();
+
+        // Reasonably simple query to collect all of the data we're interested in
+        $authors = DB::select('
+            SELECT ja.*, a.*, as1.setting_value AS affiliation, as2.setting_value AS familyName, as3.setting_value AS givenName,
+                SUBSTRING(as2.setting_value, 1, 1) AS `letter`
+            FROM journal_authors ja
+            INNER JOIN authors a ON a.author_id = ja.author_id
+            INNER JOIN author_settings as1 ON as1.author_id = a.author_id AND as1.setting_name = "affiliation"
+            INNER JOIN author_settings as2 ON as2.author_id = a.author_id AND as2.setting_name = "familyName"
+            INNER JOIN author_settings as3 ON as3.author_id = a.author_id AND as3.setting_name = "givenName"
+            WHERE context_id = ?
+            ORDER BY familyName, givenName ASC    
+        ', [ $context->getId() ]);
+        $templateMgr->assign('authors', $authors);
 
         return $templateMgr->display(
             $this->plugin->getTemplateResource(
--- templates/listAuthors.tpl
+++ templates/listAuthors.tpl
@@ -4,7 +4,24 @@
 	<div class="container-fluid container-page container-narrow">
 		{include file="frontend/components/headings.tpl" currentTitleKey="plugins.authorListing.heading"}
 
-		{$currentContext->getLocalizedSetting('about')}
+		<div class="row">
+			<div>
+			{capture assign="lastLetter"}0{/capture}
+			{foreach from=$authors item=author}
+				{if $lastLetter != $author->letter}
+					</div>
+					<div class="col-md-3 my-3">
+						<h2>{$author->letter}</h2>
+				{/if}
+				<a class="d-block"
+						title="{$author->affiliation|escape:'htmlall'}"
+						href="{url page="search"}?authors={$author->givenName|escape:'url'} {$author->familyName|escape:'url'}">
+					{$author->familyName}, {$author->givenName}
+				</a>
+				{capture assign="lastLetter"}{$author->letter}{/capture}
+			{/foreach}
+			</div>
+		</div>
 	</div>
 </main><!-- .page -->
 
